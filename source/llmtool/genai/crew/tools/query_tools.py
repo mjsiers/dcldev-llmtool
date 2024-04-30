a0386cb7da59323dc879e413eb806cafc5bbecd7
Michael Siers <msiers@foxotechnologies.com>
2024-04-30 10:23:52 -0500

    Updated query tools.

--- source/llmtool/genai/crew/tools/query_tools.py
+++ source/llmtool/genai/crew/tools/query_tools.py
@@ -1,9 +1,9 @@
 import logging
-from typing import List
+from typing import List, Optional
 
 from langchain.tools import tool
 
-from ....data.query import search_embeddings
+from ....data.query import search_embeddings, search_sections
 from ....settings import AppConfig
 
 # configure logging
@@ -27,27 +27,15 @@ class QueryTools:
         return results
 
     @tool("Fetch document sections from database")
-    def get_sections(self, section: str, filter: List[str]) -> List[str]:
+    def get_sections(
+        self, section: str, filter_uuids: Optional[List[str]] = None, limit: int = 10
+    ) -> List[str]:
         results: List[str] = []
-        return results
+        df = search_sections(self.config, section, filter_uuids=filter_uuids, limit=limit)
+        if df is None:
+            logger.info("get_sections: No results found.")
+            return results
 
-    @tool("Query vector database")
-    def write_file(self, data: str) -> str:
-        """Useful to write a file to a given path with a given content.
-        The input to this tool should be a pipe (|) separated text
-        of length two, representing the full path of the file,
-        including the /workdir/template, and the React
-        Component code content you want to write to it.
-        For example, `./Keynote/src/components/Hero.jsx|REACT_COMPONENT_CODE_PLACEHOLDER`.
-        Replace REACT_COMPONENT_CODE_PLACEHOLDER with the actual
-        code you want to write to the file."""
-        try:
-            path, content = data.split("|")
-            path = path.replace("\n", "").replace(" ", "").replace("`", "")
-            if not path.startswith("./workdir"):
-                path = f"./workdir/{path}"
-            with open(path, "w") as f:
-                f.write(content)
-            return f"File written to {path}."
-        except Exception:
-            return "Error with the input format for the tool."
+        logger.info("get_sections: Search results [%s].", df.shape)
+        results = df["text"].tolist()
+        return results
